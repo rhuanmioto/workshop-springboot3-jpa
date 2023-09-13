4c583afcf1ba10cc726ef27d85a7ddd3ea25d446
Rhuan Mioto <miotorhuan@gmail.com>
2023-09-13 15:30:05 -0300

    JPA reposityro, dependency injection, database seeding

---
+++ src/main/java/com/aprendendomioto/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aprendendomioto.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aprendendomioto.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
