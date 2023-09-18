4f9b4dcf5d8d10edeefb19140d2a6bb659f5ffa7
Rhuan Mioto <miotorhuan@gmail.com>
2023-09-18 17:27:44 -0300

    Oder, Instant, ISO 8601 - PART 2

--- src/main/java/com/aprendendomioto/course/entities/User.java
+++ src/main/java/com/aprendendomioto/course/entities/User.java
@@ -3,7 +3,8 @@ package com.aprendendomioto.course.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,98 +15,99 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
-private static final long serialVersionUID = 1L;
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		private String name;
-		private String email;
-		private String phone;
-		private String password;
-		
-		@OneToMany(mappedBy = "client")
-		private List<Order> orders = new ArrayList<>();
-		
-		public User() {
-		}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
 
-		public User(Long id, String name, String email, String phone, String password) {
-			super();
-			this.id = id;
-			this.name = name;
-			this.email = email;
-			this.phone = phone;
-			this.password = password;
-		}
-		
-		
-		public Long getId() {
-			return id;
-		}
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	public User() {
+	}
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
 
-		public String getName() {
-			return name;
-		}
+	public Long getId() {
+		return id;
+	}
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-		public String getEmail() {
-			return email;
-		}
+	public String getName() {
+		return name;
+	}
 
-		public void setEmail(String email) {
-			this.email = email;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public String getPhone() {
-			return phone;
-		}
+	public String getEmail() {
+		return email;
+	}
 
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-		public String getPassword() {
-			return password;
-		}
+	public String getPhone() {
+		return phone;
+	}
 
-		public void setPassword(String password) {
-			this.password = password;
-		}
-		
-		public List<Order> getOrders() {
-			return orders;
-		}
-		
-		
-		
-		@Override
-		public int hashCode() {
-			return Objects.hash(email, id, name, password, phone);
-		}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
 				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			User other = (User) obj;
-			return Objects.equals(email, other.email) && Objects.equals(id, other.id)
-					&& Objects.equals(name, other.name) && Objects.equals(password, other.password)
-					&& Objects.equals(phone, other.phone);
-		}
-
-		
-		
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
---
+++ src/main/java/com/aprendendomioto/course/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.aprendendomioto.course.services;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aprendendomioto.course.entities.Order;
+import com.aprendendomioto.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
